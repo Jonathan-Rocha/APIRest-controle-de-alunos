5c51930f820d9bf87e78b1a23df20dde8ab24a59
Jonathan-Rocha <johnrocha149@gmail.com>
2023-02-17 10:58:48 -0300

    change data type

--- src/main/java/br/com/server/domain/format/dto/ClassFormatData.java
+++ src/main/java/br/com/server/domain/format/dto/ClassFormatData.java
@@ -1,15 +1,13 @@
 package br.com.server.domain.format.dto;
 
-import java.math.BigDecimal;
-
 import br.com.server.domain.format.ClassFormat;
 import br.com.server.domain.user.User;
 
 public record ClassFormatData(
 		Long id,
 		String modality,
-		String timeMinutes,
-		BigDecimal price,
+		Integer timeMinutes,
+		Integer price,
 		User user
 ) {
 	public ClassFormatData(ClassFormat format) {
