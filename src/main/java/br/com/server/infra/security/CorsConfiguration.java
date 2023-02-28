323debd22dcaff277135f29659812435b78ec91c
Jonathan-Rocha <johnrocha149@gmail.com>
2023-02-28 17:17:25 -0300

    add http methods

--- src/main/java/br/com/server/infra/security/CorsConfiguration.java
+++ src/main/java/br/com/server/infra/security/CorsConfiguration.java
@@ -1,11 +1,8 @@
 package br.com.server.infra.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
 
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
@@ -14,6 +11,6 @@ public class CorsConfiguration implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
             .allowedOrigins("http://localhost:3000")
-            .allowedMethods(Arrays.toString(HttpMethod.values()));
+            .allowedMethods("GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "TRACE");
   }
 }
